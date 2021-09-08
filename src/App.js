4c5d9c6d2da5a3a199e6f6e91015991f5c9677cc
kovarik85 <kovarik8505@gmail.com>
2021-09-08 09:52:29 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ export default function App() {
   return (
     <div>
       <h1>Gombok már 100-adjára</h1>
-      <Button id="one" text="Gombocska" />
+      <Button id="one" text="Gombocska"/>
       <Button id="one" text="Gombocska" disabled />
       <Button id="one" text="Gombocska" />
-      <Button id="one" text="Gombocska" />
+      <Button id="one">Izéke</Button>
       <Button id="one" text="Gombocska" />
     </div>
   );
